2dc79ddf441131c2e0392ec7ed884fbe6486f388
DESKTOP-M9CCUTI\ian <ian@wherelabs.com>
2023-11-18 15:11:25 +0100

    amux glitch test

--- amux.c
+++ amux.c
@@ -15,17 +15,17 @@ void amux_sweep(void)
     adc_select_input(AMUX_OUT_ADC);
     for(int i=0; i<HW_ADC_MUX_COUNT; i++)
     {
-        shift_adc_select(15); //to clear any charge from a floating pin
-        busy_wait_us(10);
+        //shift_adc_select(15); //to clear any charge from a floating pin
+        //busy_wait_us(10);
         shift_adc_select(i);
-        busy_wait_us(1);
+        busy_wait_us(150);
         hw_adc_raw[i]=adc_read();
         hw_adc_voltage[i]=hw_adc_to_volts_x2(i); //these are X2 because a resistor divider /2
     }
     
-    shift_adc_select(15); //to clear any charge from a floating pin
+    //shift_adc_select(15); //to clear any charge from a floating pin
     adc_select_input(CURRENT_SENSE_ADC);
-    busy_wait_us(1);
+    busy_wait_us(100);
     hw_adc_raw[HW_ADC_CURRENT_SENSE]=adc_read();
     hw_adc_voltage[HW_ADC_CURRENT_SENSE]=hw_adc_to_volts_x1(HW_ADC_CURRENT_SENSE); 
 

9dee6d69b487ec5664213d731a5b6c9467b1aa4f
DESKTOP-M9CCUTI\ian <ian@wherelabs.com>
2024-03-29 18:37:02 +0100

    Link to last forum post with most recent firmware

--- commands/global/cmd_mcu.c
+++ commands/global/cmd_mcu.c
@@ -20,7 +20,7 @@ void cmd_mcu_jump_to_bootloader(void){
 void cmd_mcu_jump_to_bootloader_handler(struct command_result *res){
 
     printf("Jump to bootloader\r\n");
-    printf("Firmware download:\r\nhttps://forum.buspirate.com/t/bus-pirate-5-auto-build-main-branch\r\n");
+    printf("Firmware download:\r\nhttps://forum.buspirate.com/t/bus-pirate-5-auto-build-main-branch/20/999999\r\n");
     printf("Hardware revision: %d\r\n", system_config.hardware_revision);
     printf("Firmware file: bus_pirate5_rev%d.uf2\r\n", system_config.hardware_revision);
     printf("A USB disk named \"RPI-RP2\" will appear\r\nDrag a firmware file to the disk to upgrade\r\n");

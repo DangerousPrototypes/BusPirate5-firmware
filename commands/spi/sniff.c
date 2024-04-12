6f1e888a1c8c687ccbd80c2cd087f24e6303687c
DESKTOP-M9CCUTI\ian <ian@wherelabs.com>
2024-04-12 15:08:22 +0200

    duplex SPI sniff

--- commands/spi/sniff.c
+++ commands/spi/sniff.c
@@ -49,12 +49,27 @@ static PIO pio;
 static uint pio_state_machine;
 static uint pio_loaded_offset;
 
+static PIO pio;
+static uint pio_state_machine_d1;
+static uint pio_loaded_offset_d1;
+
 bool pio_read(uint32_t *raw){
     if(pio_sm_is_rx_fifo_empty(pio, pio_state_machine)){
         return false;
     }
     // 8-bit read from the uppermost byte of the FIFO, as data is left-justified
     (*raw) = pio->rxf[pio_state_machine];
+    //TODO: change this based on UART settings
+    //Detect parity error?
+    return true;
+}
+
+bool pio_read_d1(uint32_t *raw){
+    if(pio_sm_is_rx_fifo_empty(pio, pio_state_machine_d1)){
+        return false;
+    }
+    // 8-bit read from the uppermost byte of the FIFO, as data is left-justified
+    (*raw) = pio->rxf[pio_state_machine_d1];
     //TODO: change this based on UART settings
     //Detect parity error?
     return true;
@@ -68,24 +83,36 @@ void sniff_handler(struct command_result *res){
     pio=M_UART_PIO;
     pio_state_machine=M_UART_PIO_SM;
     pio_loaded_offset = pio_add_program(pio, &spisnif_program);
-    spisnif_program_init(pio, pio_state_machine, pio_loaded_offset, bio2bufiopin[BIO3], bio2bufiopin[BIO0], bio2bufiopin[BIO1]);
+    spisnif_program_init(pio, pio_state_machine, pio_loaded_offset, bio2bufiopin[BIO3], bio2bufiopin[BIO0], bio2bufiopin[BIO2]);
+
+    pio_state_machine_d1=M_UART_PIO_SM-1;
+    pio_loaded_offset_d1 = pio_add_program(pio, &spisnif_2_program);
+    spisnif_2_program_init(pio, pio_state_machine_d1, pio_loaded_offset_d1, bio2bufiopin[BIO3], bio2bufiopin[BIO1], bio2bufiopin[BIO2]);
+
+
 
     static const char pin_labels[][5] = {"DAT0","DAT1","SCLK","SCS"}; 
     
     system_bio_claim(true, BIO0, BP_PIN_MODE, pin_labels[0]);
-    system_bio_claim(true, BIO1, BP_PIN_MODE, pin_labels[2]);
+    system_bio_claim(true, BIO1, BP_PIN_MODE, pin_labels[1]);
+    system_bio_claim(true, BIO2, BP_PIN_MODE, pin_labels[2]);
     system_bio_claim(true, BIO3, BP_PIN_MODE, pin_labels[3]);
     printf("Any key to exit\r\n");
     while(true){
         if(pio_read(&value)){
-            printf("%d\r\n", value);
+            printf("%d ", value);
+        }
+        if(pio_read_d1(&value)){
+            printf("(%d) ", value);
         }
         char c;
         if(rx_fifo_try_get(&c)) break;
     }
     pio_remove_program(pio, &spisnif_program, pio_loaded_offset);
+    pio_remove_program(pio, &spisnif_2_program, pio_loaded_offset_d1);
     system_bio_claim(false, BIO0, BP_PIN_MODE, 0);
     system_bio_claim(false, BIO1, BP_PIN_MODE, 0);
+    system_bio_claim(false, BIO2, BP_PIN_MODE, 0);
     system_bio_claim(false, BIO3, BP_PIN_MODE, 0);
 }
 

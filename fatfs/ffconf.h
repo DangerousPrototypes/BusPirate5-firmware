94c90f88fb721862b87642bfe69b905760bb146d
Patrick Dussud <phdussud@hotmail.com>
2024-04-23 08:40:36 -0700

    Turn FF reentrancy on

--- fatfs/ffconf.h
+++ fatfs/ffconf.h
@@ -244,9 +244,10 @@
 /      lock control is independent of re-entrancy. */
 
 /* #include <somertos.h>	// O/S definitions */
-#define FF_FS_REENTRANT 0
+#include "pico/mutex.h"
+#define FF_FS_REENTRANT 1
 #define FF_FS_TIMEOUT   1000
-#define FF_SYNC_t       HANDLE
+#define FF_SYNC_t mutex_t*
 /* The option FF_FS_REENTRANT switches the re-entrancy (thread safe) of the FatFs
 /  module itself. Note that regardless of this option, file access to different
 /  volume is always re-entrant and volume control functions, f_mount(), f_mkfs()

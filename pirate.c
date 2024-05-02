cc65ac14a04096b7226960e847e67f3ab9bad2e7
DESKTOP-M9CCUTI\ian <ian@wherelabs.com>
2024-05-02 12:37:01 +0200

    Run button.scr when button is pressed

--- pirate.c
+++ pirate.c
@@ -25,6 +25,7 @@
 #include "ui/ui_prompt.h"
 #include "ui/ui_process.h"
 #include "ui/ui_flags.h"
+#include "ui/ui_button.h"
 #include "commands/global/freq.h"
 #include "queue.h"
 #include "usb_tx.h"
@@ -267,8 +268,8 @@ int main(){
                     printf("\r\n\r\nVT100 compatible color mode? (Y/n)> "); 
                 }
                 //printf("\r\n\r\nVT100 compatible color mode? (Y/n)> "); 
-                break;
-            
+                button_irq_enable(0, &button_irq_callback); //enable button interrupt
+                break;                 
             case BP_SM_GET_INPUT:
                 //helpers_mode_periodic();
                 //it seems like we need an array where we can add our function for periodic service?
@@ -295,7 +296,13 @@ int main(){
                         }
                         printf("\r\n");
                         bp_state=BP_SM_PROCESS_COMMAND;
+                        button_irq_disable(0);
                         break;
+                }
+                if(button_check_irq(0)){
+                    button_irq_disable(0);
+                    ui_button_exec(); //if button pressed, run button.scr script
+                    bp_state=BP_SM_COMMAND_PROMPT;  //return to command prompt
                 }
                 break;
             
@@ -316,6 +323,7 @@ int main(){
                     screensaver = add_alarm_in_ms(system_config.lcd_timeout*300000, ui_term_screensaver_enable, NULL, false);
                 }
                 bp_state=BP_SM_GET_INPUT;
+                button_irq_enable(0, &button_irq_callback);
                 break;
             
             default:
---
+++ ui/ui_button.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include <stdint.h>
+#include "pirate.h"
+#include "system_config.h"
+#include "pirate/button.h"
+#include "opt_args.h" // File system related
+#include "fatfs/ff.h" // File system related
+#include "pirate/storage.h" // File system related
+#include "ui/ui_cmdln.h" // This file is needed for the command line parsing functions
+#include "ui/ui_term.h"    // Terminal functions
+#include "ui/ui_process.h"
+#include "usb_rx.h"
+#include "usb_tx.h"
+#include "bytecode.h"
+#include "modes.h"
+
+// non zero return value indicates error
+bool ui_button_exec(void){
+
+    FIL fil;		/* File object needed for each open file */
+    FRESULT fr;     /* FatFs return code */    
+    char file[512];
+    fr = f_open(&fil, "button.scr", FA_READ);	
+    if (fr != FR_OK) {
+        printf("button.scr not found\r\n");
+        return true;
+    }
+
+    printf("\r\n");
+    while (f_gets(file, sizeof(file), &fil)) {
+        if(file[0]=='#'){ //comment line TODO: make a little more robust against leading whitespace
+            printf("%s%s%s\r",ui_term_color_info(), file, ui_term_color_reset());
+        }else{   
+            cmdln_next_buf_pos();
+            if(system_config.subprotocol_name){
+                printf("%s%s-(%s)>%s ", ui_term_color_prompt(), modes[system_config.mode].protocol_name, system_config.subprotocol_name, ui_term_color_reset());
+            }else{
+                printf("%s%s>%s ", ui_term_color_prompt(), modes[system_config.mode].protocol_name, ui_term_color_reset());
+            }
+            for(uint32_t i=0; i<sizeof(file); i++){
+                if(file[i]=='\r' || file[i]=='\n' || file[i]=='\0') break;
+                rx_fifo_add(&file[i]); 
+            }
+            while(ui_term_get_user_input()!=0); //nothing left to shove in the command prompt
+            printf("\r\n");
+            bool error=ui_process_commands();
+        }
+    }
+    printf("\r\n");
+    /* Close the file */
+    f_close(&fil);  
+    return false;
+}

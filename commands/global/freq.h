dfbc11aa27146eb78a82a8272afc7870c0928b1b
Patrick Dussud <phdussud@hotmail.com>
2024-05-06 19:08:25 -0700

    freq PIO prototype

--- commands/global/freq.h
+++ commands/global/freq.h
@@ -5,8 +5,7 @@ uint32_t freq_measure(int32_t pin, int refresh);
 uint32_t freq_configure_disable(void);
 uint32_t freq_configure_enable(void);
 
-float freq_measure_period(uint gpio);
-float freq_measure_duty_cycle(uint gpio);
+float freq_measure_period_and_duty_cycle(uint gpio, float *duty);
 void freq_display_hz(float* freq_hz_value, float* freq_friendly_value, uint8_t* freq_friendly_units);
 void freq_display_ns(float* freq_ns_value, float* period_friendly_value, uint8_t* period_friendly_units);
 

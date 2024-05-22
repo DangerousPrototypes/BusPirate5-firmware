de32ed8f12189036653c93f9278b6ebd95309b33
Wyatt Neal <wyatt.neal+git@gmail.com>
2024-05-21 21:24:38 -0400

    one of these was not like the others

--- ui/ui_help.c
+++ ui/ui_help.c
@@ -48,13 +48,14 @@ void ui_help_options(const struct ui_help_options (*help), uint32_t count) {
 }
 
 void ui_help_usage(const char * const flash_usage[], uint32_t count){
-	printf("usage:\r\n");
-	for(uint32_t i=0; i<count; i++)	{
-		printf("%s%s%s\r\n", 
-			ui_term_color_info(), 
-			flash_usage[i], 
-			ui_term_color_reset());
-	}
+    printf("usage:\r\n");
+    for(uint32_t i=0; i<count; i++)	{
+        printf("%s%s%s\r\n",
+            ui_term_color_info(),
+            flash_usage[i],
+            ui_term_color_reset()
+        );
+    }
 }
 
 bool ui_help_show(bool help_flag, const char * const usage[], uint32_t count_of_usage, const struct ui_help_options *options, uint32_t count_of_options){

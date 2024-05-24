c13e7e80df28f97f95f608d77ba24a9312483a85
Henry Gabryjelski <henrygab@users.noreply.github.com>
2024-05-23 21:35:38 -0700

    reduce complexity while retaining ability to match messages

--- pirate/intercore_helpers.c
+++ pirate/intercore_helpers.c
@@ -4,30 +4,51 @@
 
 
 // This wrapper is a response to manual code review noting
-// at least two coding patterns, neither of which is all
-// that safe...
-// Using a wrapper also makes it easier to later
-// change to a more robust cross-core communication
-// with minimal code changes elsewhere.
+// at least two coding patterns, neither of which provided
+// any indication when things were awry.
+//
+// Using this wrapper increases the detectability of code
+// errors, and also makes it easier to later change to
+// alternative cross-core communication methods if needed.
 
 
-void icm_core0_send_message(uint8_t message_id) {
-    static uint8_t static_message_count = 0u;
-
+void icm_core0_send_message(bp_icm_message_t message_id) {
     // NOTE: although split into multiple lines for readability,
     //       the compiler optimizes this quite well!
 
-    // encode a message counter into the value,
-    // but keep top byte as zero to avoid overlap with RP2040 valid memory locations
-    uint32_t icm_value = ++static_message_count;
+    static_assert(sizeof(message_id) <= 2); // else have to update encoding / decoding
 
-    // shift those bits to avoid overlap with the encoded message_id parameter
-    static_assert(sizeof(message_id) < sizeof(icm_value));
-    icm_value <<= sizeof(message_id)*8;
+    static uint8_t static_message_count = 0u;
 
-    // add in the message_id
-    icm_value |= message_id;
+    // Basic concepts:
+    // 1. Encode a counter within the message ID.
+    //    This prevents various code errors from
+    //    causing intercore synchronization to be lost.
+    //    The counter is incremented each time core0
+    //    sends a message using this API, and the wait
+    //    ensure this same message is received confirming
+    //    the message was processed.
+    // 2. Ensure that the message ID is not a valid pointer.
+    //    Per the RP2040 memory map, the range 0x60000000..0xCFFFFFFF
+    //    is unmapped, and will cause a bus error if accessed.
+    //
+    // RP2040 uses a `uint32_t` value for the message.
+    // Currently this is functionally equivalent to:
+    // typedef struct {
+    //     uint32_t map   : 8 = 0x80u;      // constant
+    //     uint32_t cnt   : 8 = counter;    // incremented each time a message is sent
+    //     uint32_t RFU   : 8 = 0x00u;      // reserved for future use
+    //     uint32_t Value : 8 = message_id; // actual message
+    // } icm_message_t;
+    // 
 
+    // Values in range 0x60000000 to 0xCFFFFFFF are unmapped on RP2040
+    // Choosing 0x80000000..0x80FFFFFF for intercore messages
+    ++static_message_count;
+    uint32_t icm_value =
+        (uint32_t)0x80000000u
+        |  (((uint32_t)static_message_count) << 16)
+        |  message_id;
     multicore_fifo_push_blocking(icm_value);
 
     do {
--- pirate/intercore_helpers.h
+++ pirate/intercore_helpers.h
@@ -11,21 +11,23 @@
 //
 
 // Having defined names (rather than magic numbers) improves readability of the code.
-#define BP_ICM_INIT_CORE1            ((uint8_t)0xA5) // Used to synchronize Core0 and Core1 intialization.
-#define BP_ICM_DISABLE_LCD_UPDATES   ((uint8_t)0xF0) // disables LCD updates / IRQ
-#define BP_ICM_ENABLE_LCD_UPDATES    ((uint8_t)0xF1) // enables LCD updates / IRQ
-#define BP_ICM_FORCE_LCD_UPDATE      ((uint8_t)0xF2) // enable LCD updates / IRQ and force an update
-#define BP_ICM_DISABLE_RGB_UPDATES   ((uint8_t)0xF3) // disables RGB updates / IRQ
-#define BP_ICM_ENABLE_RGB_UPDATES    ((uint8_t)0xF4) // enables RGB updates / IRQ
+typedef uint8_t bp_icm_message_t;
+#define BP_ICM_INIT_CORE1            ((bp_icm_message_t)0xA5) // Used to synchronize Core0 and Core1 intialization.
+#define BP_ICM_DISABLE_LCD_UPDATES   ((bp_icm_message_t)0xF0) // disables LCD updates / IRQ
+#define BP_ICM_ENABLE_LCD_UPDATES    ((bp_icm_message_t)0xF1) // enables LCD updates / IRQ
+#define BP_ICM_FORCE_LCD_UPDATE      ((bp_icm_message_t)0xF2) // enable LCD updates / IRQ and force an update
+#define BP_ICM_DISABLE_RGB_UPDATES   ((bp_icm_message_t)0xF3) // disables RGB updates / IRQ
+#define BP_ICM_ENABLE_RGB_UPDATES    ((bp_icm_message_t)0xF4) // enables RGB updates / IRQ
 
 // While core1 has a single point where these messages are received / responded to,
 // core0 sends the messages from many files.
 // Funnel into low-overhead wrapper to catch errors in cross-core synchronization,
 // because debugging such issues is ... non-trivial.
-void icm_core0_send_message(uint8_t message_id);
+void icm_core0_send_message(bp_icm_message_t message_id);
 
 
 // Ultra-low overhead ... maybe even zero overhead with full optimizations.
-inline uint8_t icm_get_message(uint32_t icm_value) {
-    return (uint8_t)(icm_value & 0xFFu);
+inline bp_icm_message_t icm_get_message(uint32_t icm_raw_value) {
+    static_assert(sizeof(bp_icm_message_t) == 1); // else have to update this to mask differently
+    return (bp_icm_message_t)(icm_raw_value & 0xFFu);
 }

cdef28f8345767b08ebf60e6c4b3a83884901f89
Patrick Dussud <phdussud@hotmail.com>
2024-06-17 10:30:04 -0700

    Logic analyzer code cleanup, fix potential race condition, edge triggering support
    sump support for edge triggering
    pio code cleanup (shorter) and edge triggering support

--- mode/logicanalyzer.h
+++ mode/logicanalyzer.h
@@ -1,9 +1,10 @@
 void la_test_args (struct command_result *res);
 bool logicanalyzer_setup(void);
+void restart_dma();
 int logicanalyzer_status(void);
 uint8_t logicanalyzer_dump(uint8_t *txbuf);
 bool logic_analyzer_is_done(void);
-bool logic_analyzer_arm(float freq, uint32_t samples, uint32_t trigger_mask, uint32_t trigger_direction);
+bool logic_analyzer_arm(float freq, uint32_t samples, uint32_t trigger_mask, uint32_t trigger_direction, bool edge);
 bool logic_analyzer_cleanup(void);
 void logicanalyzer_reset_led(void);
 void la_periodic(void);

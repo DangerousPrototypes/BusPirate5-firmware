d16f290edbabff0321d8893d139d28b53705f972
Henry Gabryjelski <henrygab@users.noreply.github.com>
2024-10-01 22:14:02 -0700

    Final cleanup

--- translation/base.c
+++ translation/base.c
@@ -9,72 +9,79 @@
 #include "translation/bs-ba.h"
 #include "translation/it-it.h"
 
-// TODO: define a type for translation table (just to make it more readable than `const char * const *`)
-// TODO: how to typedef a constant array of pointers to constant characters?
-//       gcc warns about the assignment of `const char * const (*)[430]` to target type `const char * const *`
 typedef const char * const translation_table_t[T_LAST_ITEM_ALWAYS_AT_THE_END];
 
-// TODO: check how json2h.py pulls strings from json files, and if NULL
-//       can be used to indicate that a string has no translation.
+// To add a new translation / dialect / language:
 // ======================================================================
-// To add a new translation:
-// ======================================================================
-// 1. Edit en-us.h to add a new translation string for the new language.
-//    (This is necessary due to current config menu structure...)
-//    example:
+//
+// 1. Edit en-us.h to add a string for the new dialect.
+//    The enumeration name MUST begin with `T_CONFIG_LANGUAGE_`.
+//    For example, to add Esperanto as a new option, it might look like:
 //        [T_CONFIG_LANGUAGE_ESPERANTO] = "Lingvo - esperanto (Mondo)",
 //
-// 2. Edit json2h.py to:
-//    - include the new language enumeration in the tuple of
-//      DO_NOT_TRANSLATE_THESE_STRINGS.  This will help ensure
-//      the new language selection is not accidentally translated.
-//    - if "language" in that language is not similar to an existing
-//      string in T_CONFIG_LANGUAGE, add as option.
-//    example:
-//      Currently, [T_CONFIG_LANGUAGE]="Language / Jezik / Lingua / 语言"
-//      Do not add pl-pl "Język" to that string, as it is similar enough
-//      to the bs_Latn_BA "Jezik" string.
-// 
-// 3. Create a new .json file in the translation directory
+// 2. Review [T_CONFIG_LANGUAGE] in en-us.h.
+//    Review the new dialect's word for "language".
+//    If significantly different from existing options,
+//    modify the string to include the new translation's word for "language".
+//    For example, if language in Esperanto was `XYZZY`, it might change:
+//        [T_CONFIG_LANGUAGE] = "Language / Jezik / Lingua / 语言"
+//    to:
+//        [T_CONFIG_LANGUAGE] = "Language / Jezik / Lingua / 语言 / XYZZY"
+//
+// 3. Create a new .json file in the translation directory, with the base
+//    filename corresponding to the IETF language tag for the new dialect.
 //    - Recommand starting with en-us.json as a template.
 //    - Name the file using the IETF language tag corresponding
 //      to the language of the translation.
-//    example:
+//    For example, if adding Esperanto:
 //        cp en-us.json eo-001.json
 //
-// 4. Populate at least the strings that are translated
+// 4. Translate or remove the enumerated string from the .json file
 //    - There is no requirement to translate all strings.
 //    - Translated strings must be in the same order as the en_us file.
-//    - If a string is not translated, either exclude from the json file
-//      entirely or set its entry to null -- the generated header will
-//      then automatically get the most recent version of the en-US string.
-//    example:
+//    - If a string is not translated, simply exclude that entry from
+//      the json file entirely.
+//    - TODO: check if can explicitly set the entry to null
+//    - Missing or entries storing null (special json value) will
+//      automatically load the most recent version of the en-US string.
+//    Made-up words for example Esperanto translation:
 //        {
 //            "T_CONFIG_LANGUAGE_ESPERANTO": null,
-//            "T_CONFIG_PIN": "PiN"
+//            "T_CONFIG_PIN": "PyN"
 //        }
 //
 // 5. Edit `base.ht` to add the new language to the enum T_language_t.
 //    - This will become part of the new generated `base.h` in the next step.
-//    example:
+//    - Name should be `language_idx_` followed by the IETF language tag
+//      (replacing dash with underscore)
+//    Continuing this example of adding esperanto:
 //        language_idx_eo_001, // Esperanto
 //
-// 6. Run `json2h.py` in the translation directory.
-//    - Extracts key/value pairs from `en-us.h`
-//    - Generates `en-us.json` (for use as a template for new languages)
-//    - Generates `base.h` from `base.ht` (template).
-//    - Generates `.h` files for each `.json` file in the directory.
+// 6. Run `python ./json2h.py` while in the translation directory.
+//    - This parses `en-us.h` to extracts key/value pairs for each string.
+//    - Generates `en-us.json` (for use as a template for new languages).
+//    - Generates `base.h` from `base.ht` (template) to define enumeration of string IDs.
+//    - Generates `.h` files for each `.json` file in the directory
+//      (except `en-us.json`, of course, since that was just generated)
 //
-// 7. Manually update the table below to include the mapping from the
-//    new language idx in base.h (base.ht) to the variable in the generated
-//    translation table header file.
+// 7. Finally, manually update the table below to include the new `T_language_t`
+//    that was added to `base.ht` above, and the name of the table in the new
+//    dialect's generated header.
+//    Continuing the example of adding Esperanto, the added line might be:
+//     [language_idx_eo_001] = &eo_001, // eo-001 aka Esperanto
+//
+// TODO: lots of changes to make this more automatic at compile time
+
 
 // BUGBUG: Rename the source .json files to correspond to the proper IETF language tag, and regenerate the .h files.
-// BUGBUG: Update json2h.py to prefix the variable name ... e.g., with `t_translation_table_` or similar.
-// N.B. - Because this takes address of a variable, this goes into RAM, not ROM ... but only pointers, so only four bytes per language.
+// BUGBUG: Update json2h.py to prefix the resulting variable name ... e.g., with `t_translation_table_` or similar.
+
+
 
 static language_idx_t current_language = language_idx_en_us;
 
+// N.B. - The translation table itself takes up RAM because it stores address of other variables.
+//        But this is only four bytes per language.  The actual string array should be in ROM.
 static const translation_table_t* translation_tables[COUNT_OF_LANGUAGE_IDX] = {
     [language_idx_en_us]       = &en_us,       // en_US
     [language_idx_pl_pl]       = &pl_pl,       // pl_PL,
@@ -85,7 +92,6 @@ static const translation_table_t* translation_tables[COUNT_OF_LANGUAGE_IDX] = {
 
 #define UTF8_POOP_EMOJI_STRING "\xF0\x9F\x92\xA9" // poop emoji (💩) as a UTF-8 encoded, null-terminated string
 
-//TODO: lots of changes to make this more automatic at compile time
 void translation_set(language_idx_t language)
 {
     if (language >= COUNT_OF_LANGUAGE_IDX) {
@@ -95,8 +101,6 @@ void translation_set(language_idx_t language)
     current_language = language;
 }
 
-//TODO: migrate from direct access to `t[]`, to using this accessor function instead.
-//      this will allow for greater safety, troubleshooting, and logging of errors.
 const char * GET_T(enum T_translations index) {
     const char * result = NULL;
     if (index < T_LAST_ITEM_ALWAYS_AT_THE_END) {

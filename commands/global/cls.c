91298e6ebee99c230490888c7c75bacca05d033b
DESKTOP-M9CCUTI\ian <ian@wherelabs.com>
2024-10-09 12:23:51 +0200

    cls command now reports error if not in VT100 mode.

--- commands/global/cls.c
+++ commands/global/cls.c
@@ -21,6 +21,11 @@ static const struct ui_help_options options[]={
 void ui_display_clear(struct command_result *res){
     if(ui_help_show(res->help_flag,usage,count_of(usage), &options[0],count_of(options) )) return;
 
+    if(!system_config.terminal_ansi_color){
+        printf("cls command is only supported in VT100 terminal mode\n");
+        return;
+    }
+
     ui_term_detect(); // Do we detect a VT100 ANSI terminal? what is the size?
     ui_term_init();   // Initialize VT100 if ANSI terminal
     if(system_config.terminal_ansi_color && system_config.terminal_ansi_statusbar){

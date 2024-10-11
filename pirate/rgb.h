3f07132ea2ca9340cef27aed9f8a47a9269c56ef
Henry Gabryjelski <henrygab@users.noreply.github.com>
2024-10-10 21:56:29 -0700

    A couple more utility functions to set the RGB pixels.

--- pirate/rgb.h
+++ pirate/rgb.h
@@ -20,8 +20,15 @@ static_assert(MAX_LED_EFFECT-1 == LED_EFFECT_PARTY_MODE, "LED_EFFECT_PARTY_MODE 
 
 // TODO: review and provide a more useful client-focused API.
 // TODO: adjust to use RGB color type instead of uint32_t.
+
+// TODO: some method to "claim" subset of the pixels for a particular effect
+//       Examples:
+//       * top or bottom row (e.g., for a progress bar)
+//       * all pixels (e.g., for clock or infinite progress bar)
 void rgb_init(void);
 void rgb_put(uint32_t color);
 void rgb_irq_enable(bool enable);
 void rgb_set_all(uint8_t r, uint8_t g, uint8_t b);
+void rgb_set_masked(uint8_t r, uint8_t g, uint8_t b, uint32_t pixel_mask);
+void rgb_set_pixels(uint32_t* colors, uint32_t count);
 void rgb_set_effect(led_effect_t new_effect);

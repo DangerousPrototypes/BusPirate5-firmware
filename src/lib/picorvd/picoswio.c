7a91d0dd0b5deca6def0f4c1b89590b258b0fd7e
DESKTOP-7VKKLTO\ian <ian@wherelabs.com>
2025-03-07 11:27:41 +0100

    Explain excluded pulldowns on RP2350 platform.

--- src/lib/picorvd/picoswio.c
+++ src/lib/picorvd/picoswio.c
@@ -71,10 +71,10 @@ void ch32vswio_reset(int pin, int dirpin) {
    sm_config_set_clkdiv(&c, clock_get_hz(clk_sys)/10000000);
 
 #if RPI_PLATFORM == RP2040
- // TODO: Document what is done on RP2350, and why this works.
- //       Is there intentional split ownership in RP2040 of a GPIO used by PIO?
- //       Is there a bug on RP2350 that prevents this?
- //       Is this a workaround for a bug on RP2040?
+// RP2350 has defective pull-downs (bug E9) that latch up
+// RP2350 baords have extra large external pull-downs to compensate
+// RP2040 has working pull-downs
+// Don't enable pin pull-downs on RP2350
  gpio_pull_down(pin);
 #endif
   pio_sm_set_pindirs_with_mask(pio_config.pio, pio_config.sm, 0, (1u<<pin)); //read pins to input (0, mask)  

1af6946950c78558266b43b0a0a6453dca70c294
DESKTOP-7VKKLTO\ian <ian@wherelabs.com>
2025-05-05 18:03:29 +0200

    enable PWM PSU flag

--- src/pirate.h
+++ src/pirate.h
@@ -111,6 +111,7 @@
         #define BP_HW_FALA_BUFFER 1
         #define BP_HW_RP2350_E9_BUG 1
         #define BP_HW_PSRAM 1
+        #define BP_HW_PSU_PWM 1
     #else
         #error "Unknown platform version in pirate.h"
     #endif
